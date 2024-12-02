5d5680bf431b6a99bfa35c7c5724560584ba4c8f
Nguyễn Văn Duy <nvduy15062004@gmail.com>
2024-12-03 02:51:22 +0700

    Add File

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# Technology-Sales-Sample" 
+
+Đây là website mang tính chất học tập không mang tính chất thương mại.
---
+++ assets/js/dungchung.js
@@ -0,0 +1,52 @@
+function navtop() {
+    document.write(`<div id="nav__top">
+                <section>
+                    <div class="social__nav">
+                        <a href="https://www.facebook.com/" class="fa-brands fa-facebook-f"></a>
+                        <a href="https://www.youtube.com/" class="fa-brands fa-youtube"></a>
+                        <a href="https://www.instagram.com/" class="fa-brands fa-instagram"></a>
+                        <a href="https://www.tiktok.com/" class="fa-brands fa-tiktok"></a>
+                    </div>
+
+                    <ul class="quickly__nav flexContain">
+                        <li>
+                            <a href="./index.html">
+                                <i class="fa-solid fa-house-chimney"></i>
+                                Trang Chủ
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./tintuc.html">
+                                <i class="fa-solid fa-newspaper"></i>
+                                Tin Tức
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./tuyendung.html">
+                                <i class="fa-solid fa-handshake"></i>
+                                Tuyển Dụng
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./gioithieu.html">
+                                <i class="fa-solid fa-circle-info"></i>
+                                Giới Thiệu
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./baohanh.html">
+                                <i class="fa-solid fa-wrench"></i>
+                                Bảo Hành
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./lienhe.html">
+                                <i class="fa-solid fa-phone"></i>
+                                Liên Hệ
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="clear"></div>
+                </section>
+            </div>`);
+}
